fc52a6f503bd81bcef449f582a26b3ceebe6da37
Joshua Gassama <JoshuaGassama@yahoo.com>
2014-10-03 13:50:52 -0700

    First commit

---
+++ public_html/js/snake.js
@@ -0,0 +1,29 @@
+var snake;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+gameInitialize();
+gameDraw();
+
+
+function gameInitialize() {
+     var canvas = document.getElementById("game-screen");
+     context = canvas.getContext ("2d");
+
+     screenWidth= window.innerWidth;
+     screenHeight=window.innerHeight;
+
+     canvas.width = screenWidth;
+     canvas.height = screenHeight;
+     }
+
+function gameLoop() {
+
+}
+
+function  gameDraw() {
+   context.fillstyle = "rgb (180, 250, 213)";
+   context. fillRect (0, 0, screenWidth, screenHeight);
+   }
