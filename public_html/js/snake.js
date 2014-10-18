99a0a2c4606908b77af82e920152d8bdd99f0885
Joshua Gassama <JoshuaGassama@yahoo.com>
2014-10-17 19:59:18 -0700

    I'm still working on the projects.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -2,7 +2,7 @@ var snake;
 var snakeLenght;
 var snakeSize;
 
-var  context;
+var context;
 var screenWidth;
 var screenHeight;
 
@@ -13,27 +13,42 @@ function gameInitialize() {
     var canvas = document.getElementById("game-screen");
     context = canvas.getContext("2d");
 
-    screenWidth = window.innerWidth;
-    screenHeight = window.innerHeight;
+screenWidth = window.innerWidth;
+screeHeight = window.innerHeight;
 
-    canvas.width = screenWidth;
-    canvas.height = screenHeight;
-    }
+canvas.width = screenWidth;
+canvas.height = screenHeight;
+}
 
-function gameLoop () {
+function gameLoop() {
 
 }
 
 function gameDraw() {
-    context.fillStyle = "rgb (180; 250; 213)";
-    context.fillRect(0, 0, screenWidth, screenHeight);
+    context.fillStyle = "rgb(180, 250, 213)";
+    context.fillRect (0,0, screenWidth, screenHeight);
+    }
+
+function snakeInitialize () {
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+
+    for(var index = 0; index < snakeLength; index++ ) {
+        snake.push( {
+            x: index,
+            y: 0
+        });   
+    }
+    }
+
+function snakeDraw() {
+    for (var index = 0; index < snake.length; index++) {
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
     }
     
-function snakeInitialize() { 
-    
-    }
-    
-    function snakeDraw (){
-    
-    
-}
+   function snakeUpdate() {
+       
+   }
