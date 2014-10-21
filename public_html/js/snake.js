d8408e7da4eac3ca3ca3694415a99cabff46e09b
Joshua Gassama <JoshuaGassama@yahoo.com>
2014-10-20 22:11:26 -0700

    On video 22. Having  problems with my code.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -8,36 +8,38 @@ var screenHeight;
 
 gameInitialize ();
 snakeInitialize();
-gameDraw ();
-snakeDraw();
+setInterval(gameLoop, 1000/30);
+
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
     context = canvas.getContext("2d");
-        
-screenWidth = window.innerWidth;
-screenHeight = window.innerHeight;
 
-canvas.width = screenWidth;
-canvas.height = screenHeight;
-}
+   screenWidth = window.innerWidth;
+   screenHeight = window.innerHeight;
 
-function gameLoop() {
+   canvas.width = screenWidth;
+   canvas.height = screenHeight;
+   }
 
-}
+     function gameLoop() {
+         gameDraw();
+      snakeUpdate();
+      snakeDraw();
+  }
 
 function gameDraw() {
     context.fillStyle = "rgb(180, 250, 213,)";
-    context.fillRect(0, 0, screenWidth, screenHeight);
-    }
+    context.fillRect (0, 0, screenWidth, screenHeight);
+}
     
 function snakeInitialize () {
     snake = [];
-    snakeLength = 5;
-    snakeSize = 100;
-
-    for(var index  = snakeLength -1; index >= 0; index--)  {
-        snake.push ({
+    snakeLength = 5;    
+    snakeSize =  20;
+    
+    for(var index = snakeLength - 1; index >= 0;  index--) {
+        snake.push( {
             x: index,
             y: 0
         });
@@ -45,12 +47,20 @@ function snakeInitialize () {
     }
 
 function snakeDraw() {
-   for(var index = 0; index < snake.length; index++) {
-       context.fillStyle = "white";
-       context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    for (var index = 0; index < snake.length; index++) {
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
     }
-    }
- 
- function snakeUpdate() {
- 
- }
+}
+
+function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+   snakeHeadX++;
+
+   var snakeTail = snake.pop();
+   snakeTail.x = snakeHeadX;
+   snakeTail.y = snakeHeadY;
+   snake.unshift(snakeTail);
+}
