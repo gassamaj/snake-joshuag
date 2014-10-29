e79734b4952712d6432b6f9f5ff5871f9f24a116
Joshua Gassama <JoshuaGassama@yahoo.com>
2014-10-28 21:58:19 -0700

    Video 50

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -216,5 +216,6 @@ function displayMenu(menu) {
 }
  
  function centerMenuPosition (menu) {
-     menu.style.top = (screenHeight / 2) + "px";
+  menu.style.top = (screenHeight / 2) - (menu.offsetHeight / 2) + "px";
+  menu.style.left = (screenWidth / 2) - (menu.offsetWidth /2) + "px";
  }
